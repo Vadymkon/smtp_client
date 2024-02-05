77898ed13d73085d9c056f45d0d4b6f27ee3cbc0
Vadymkon <vadykakonon@gmail.com>
2024-02-05 22:13:15 +0200

    First commit

---
+++ EmailGo/EmailGo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+using System.Windows;
+
+namespace EmailGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2 && args[0].Length != 0 && args[1].Length != 0)
+            {
+                SmtpClient C = new SmtpClient("smtp.gmail.com");
+                C.Port = 587;
+                C.Credentials = new NetworkCredential("vadykakonon", "code");
+                C.EnableSsl = true;
+                C.Send(new MailMessage("vadykakonon@gmail.com", args[0], args[1], $"LAB-1\n{DateTime.Now}\nKonchaVadym"));
+                MessageBox.Show("done.");
+            }
+            else
+            {
+                Console.WriteLine("SYNTAX: mail-1 <ToAddr> <Title>");
+                MessageBox.Show("failed.");
+            }
+        }
+    }
+}
